6350af9d4f78a0b5d1e38ae7b1ff8f139155834d
IM-GYURI <kgulr0517@gmail.com>
2024-05-06 15:41:34 +0900

    Refactor : Delete useless imports
    불필요한 import문 삭제

--- src/main/java/com/zerobase/reservation/dto/review/UpdateDto.java
+++ src/main/java/com/zerobase/reservation/dto/review/UpdateDto.java
@@ -2,7 +2,10 @@ package com.zerobase.reservation.dto.review;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 public class UpdateDto {
     @Getter
